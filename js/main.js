2e9951232722873c2f3673090f265125884885b4
Rodrigo Sousa <rodrigorasph@gmail.com>
2023-02-26 00:18:15 -0300

    Criacao das funções do crud

--- js/main.js
+++ js/main.js
@@ -4,7 +4,43 @@ const openModal = () => document.getElementById('modal')
       .classList.add('active')
 
 const closeModal = () => document.getElementById('modal')
-      .classList.remove('active')      
+      .classList.remove('active') 
+      
+ 
+const tempClient = {
+      nome: "del tus tus tus",
+      email: "juvenal@gmail.com",
+      celular: "6299800000",
+      cidade: "Goiania"
+}
+
+const getLocalStorage  = ()=> JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage  = (dbClient)=> localStorage.setItem("db_client", JSON.stringify(dbClient))
+
+//READ
+const readClient = ()=> getLocalStorage()
+
+//UPDATE
+const updateClient = (index, client) => {
+      const dbClient = readClient()
+      dbClient[index] = client
+      setLocalStorage(dbClient)
+  }
+
+//DELETE
+const deletClient = (index) => {
+      const dbClient = readClient()
+      dbClient.splice(index, 1) 
+      setLocalStorage(dbClient)
+}
+
+//CREATE
+const createClient = (client) => {
+      const dbClient = getLocalStorage();
+      dbClient.push(client);
+      setLocalStorage(dbClient);
+      
+}
 
 document.getElementById('cadastrarCliente')
         .addEventListener('click', openModal)
